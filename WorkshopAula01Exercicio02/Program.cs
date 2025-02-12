34123aa8824de58261c7947922decf12c2a29695
Jeferson Quaiato <jefersonquaiato@gmail.com>
2025-02-12 19:33:56 -0300

    Add project files.

---
+++ WorkshopAula01Exercicio02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Digite seu nome completo: ");
+string nomeCompleto = Console.ReadLine();
+
+Console.Write("Digite seu peso: ");
+double peso = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite sua altura em metros: ");
+double altura  = Convert.ToDouble(Console.ReadLine());
+
+double imc = peso / (altura * altura);
+
+Console.WriteLine($"\nNome: {nomeCompleto}");
+Console.WriteLine($"Peso: {peso}");
+Console.WriteLine($"Altura: {altura}");
+Console.WriteLine($"IMC: {imc:F2}");
